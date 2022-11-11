543bf723abff42b459cebc9034e63b9e2b738201
CrisMai <crismai.dev@gmail.com>
2022-11-11 14:34:19 -0300

    Criado uma requisição para serviço externo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-  return (
-    <div>
-      Olá, mundo!
-    </div>
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
+  return(
+  <div>
+    Olá, mundo!
+  </div>
   );
+
 }
