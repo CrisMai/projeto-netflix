00aa471fa16fa25d37c2846a39a62e6c433e530a
CrisMai <crismai.dev@gmail.com>
2022-11-17 14:52:56 -0300

    Criado as informações do filme destaque

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./FeaturedMovie.css";
 
 export default ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+
+    let genres = [];
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name);
+    }
+
     return (
        <section className="featured" style={{
         backgroundSize: 'cover',
@@ -11,6 +19,18 @@ export default ({item}) => {
 
         <div className="featured--vertical">
             <div className="featured--horizontal">
+                <div className="featured--name">{item.origial_name}</div>
+                <div className="featured--info">
+                    <div className="featured--points">{item.vote_average} pontos</div>
+                    <div className="featured--year">{firstDate.getFullYear()}</div>
+                    <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    <div className="featured--description">{item.overwiew}</div>
+                    <div className="featured--buttond">
+
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros</strong>{genres.join(', ')}</div>
+                </div>
+
 
             </div>
 
