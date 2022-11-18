4d6e879f20eee64c99c60483670bac6602e17470
CrisMai <crismai.dev@gmail.com>
2022-11-18 15:42:31 -0300

    Criado css para as informações exibidas no filme em destaque

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -26,8 +26,8 @@ export default ({item}) => {
                     <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     <div className="featured--description">{item.overwiew}</div>
                     <div className="featured--buttons">
-                        <a href={`/watch/${item.id}`}>Assistir</a>
-                        <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                        <a href={`/watch/${item.id}`} className="featured--watchbutton">Assistir</a>
+                        <a href={`/list/add/${item.id}`} className="feaatured--mylistbuton">+ Minha Lista</a>
 
                     </div>
                     <div className="featured--genres"><strong>Gêneros</strong>{genres.join(', ')}</div>
