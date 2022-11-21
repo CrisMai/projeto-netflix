855b6e994db9402ae235d2b5855a06f859a0eb99
CrisMai <crismai.dev@gmail.com>
2022-11-21 14:40:12 -0300

    Modificado o item descrição do filme

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -10,6 +10,11 @@ export default ({item}) => {
         genres.push(item.genres[i].name);
     }
 
+    let description = item.overwiew;
+    if (description.length > 200) {
+        description = description.substring(0, 200) + '...';
+    }
+
     return (
        <section className="featured" style={{
         backgroundSize: 'cover',
@@ -24,7 +29,7 @@ export default ({item}) => {
                     <div className="featured--points">{item.vote_average} pontos</div>
                     <div className="featured--year">{firstDate.getFullYear()}</div>
                     <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
-                    <div className="featured--description">{item.overwiew}</div>
+                    <div className="featured--description">{description}</div>
                     <div className="featured--buttons">
                         <a href={`/watch/${item.id}`} className="featured--watchbutton">Assistir</a>
                         <a href={`/list/add/${item.id}`} className="feaatured--mylistbuton">+ Minha Lista</a>
